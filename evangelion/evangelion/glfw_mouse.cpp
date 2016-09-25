a9f44cd3950535276f304d98b85932ca771ddbe9
Yeseul <lilynys13@gmail.com>
2016-09-26 05:10:41 +0900

    change color mouse position

--- evangelion/evangelion/glfw_mouse.cpp
+++ evangelion/evangelion/glfw_mouse.cpp
@@ -29,6 +29,15 @@ glfwGetCursorPos(window, &xpos, &ypos);
 */
 // scratched from https://courses.engr.illinois.edu/ece390/archive/archive-f2000/mp/mp4/anti.html
 // see 'Rasterization' part.
+bool isInsideCircle(const double x, const double y, const double x_c, const double y_c, const double r) {
+
+	const double f = (x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) - r*r;
+	if (f > 0.0) return false;
+	else {
+		printf("1\n");
+		return true;
+	}
+}
 void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
 {
 	for (int i = i0; i <= i1; i++)
@@ -89,9 +98,17 @@ void drawPolygon() {
 	drawLine(550, 200, 570, 250, 0.0f, 0.0f, 0.0f);
 	drawLine(550, 201, 570, 251, 0.0f, 0.0f, 0.0f);
 }
-void drawCircle(const double& rad, const int& i0, const int& j0, const float& red, const float& green, const float& blue) {
 
+void drawCircle(double xpos,double ypos,double rad, const int& i0, const int& j0, float red, float green, float blue){
+	bool color = isInsideCircle(xpos, 500-ypos, i0, j0, rad);
+	if (color == true)
 	{
+		red = 0.0f;
+		blue = 1.0f;
+	}
+	for(int i=0;i<20;i++)
+	{
+		rad = rad + i*0.01;
 		for (double i = 0.0; i < 360.0; i += 0.1)
 		{
 			double angle = i *  M_PI / 180;
@@ -103,18 +120,10 @@ void drawCircle(const double& rad, const int& i0, const int& j0, const float& re
 		}
 	}
 
-}
-bool isInsideCircle(const double x, const double y, const double x_c, const double y_c, const double r) {
-
-	const double f = (x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) - r*r;
-	if (f > 0.0) return false;
-	else {
-		drawCircle(50, 200, 200, 1.0f, 0.0f, 0.0f);
-		printf("1\n");
-		return true;
 	}
-}
-void drawTriangle() {
+
+/*
+void drawTriangle( {
 
 	int i_center = 100;
 	int j_center = 100;
@@ -132,6 +141,7 @@ void drawTriangle() {
 		}
 	}
 }
+*/
 void drawEmptySquare(int size, const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue) {
 	int i_center = (i0 + i1) / 2;
 	int j_center = (j0 + j1) / 2;
@@ -146,6 +156,15 @@ void drawEmptySquare(int size, const int& i0, const int& j0, const int& i1, cons
 	for (int i = i_center - size; i <= i_center + size; i++) {
 		for (int j = j_center - size; j <= j_center + size; j = j + size * 2)
 			drawPixel(i, j, red, green, blue);
+	}
+}
+void heightLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue) {
+	//for (int i = i0; i <= i1; i++)
+	//
+	for(int i=0;i<50;i++)
+	{
+
+		drawPixel(i0, j0-i, red, green, blue);
 	}
 }
 void drawOnPixelBuffer(double xpos, double ypos)
@@ -173,22 +192,30 @@ void drawOnPixelBuffer(double xpos, double ypos)
 	drawThicknerLine(thickness, i0, j0, i1, j1, 0.0f, 1.0f, 0.0f);
 	//drawSquare(size, 80+240, j0, i1 + 300, j1, 0.0f, 1.0f, 1.0f);
 	for(int i = 0; i < 8; i++) {
-		drawCircle(15+i, 200, 350, 0.0f, 1.0f, 0.0f);
+		drawCircle(0,0,15+i, 200, 350, 1.0f, 0.0f, 0.0f);
 	}
 	for(int i=0;i<5;i++)
-	drawCircle(50, 80+i*120, 350, 0.0f, 1.0f, 0.0f);
+	drawCircle(xpos,ypos,50, 80+i*120, 350, 1.0f, 0.0f, 0.0f);
 
 	for (int i = 0; i<5; i++)
-		drawCircle(50, 80 + i * 120, 150, 0.0f, 1.0f, 0.0f);
-
-	isInsideCircle(xpos,ypos,200, 300, 50);
+		drawCircle(xpos,ypos,50, 80 + i * 120, 150, 1.0f, 0.0f, 0.0f);
 
 	//if(isInsideCircle(i0 + 100, i1 + 100,50))
 	//	drawCircle(50, i0 + 100, i1 + 100, 1.0f, 0.0f, 0.0f);
 	drawEmptySquare(size, i0+370, j0+20, i1 + 100, j1, 1.0f, 0.0f, 0.0f);
-	drawLine(i0+20, j0, i0+21, j0+100, 1.0f, 0.0f, 0.0f);
-	drawTriangle();
-	drawPolygon();
+	drawLine(i0+360, j0+10, i1+360, j1+10, 1.0f, 0.0f, 0.0f);
+	drawLine(i0 + 360, j1 + 10, i1 + 360, j0 + 10, 1.0f, 0.0f, 0.0f);
+	//->
+	drawLine(i0  , j0 - 170, i1 , j0 - 170, 1.0f, 0.0f, 0.0f);
+	// A
+	drawLine(i0 + 120, j0 - 170, i1 + 100, j0 - 170, 1.0f, 0.0f, 0.0f);
+	//<-
+	drawLine(i0 + 360, j0-170, i1 + 360, j0 -170, 1.0f, 0.0f, 0.0f);
+
+	heightLine(i0 + 500, j1 + 10, i1 + 360, j0 + 10, 1.0f, 0.0f, 0.0f);
+	heightLine(i0 + 260, j1 -190, i1 + 360, j0 + 10, 1.0f, 0.0f, 0.0f);
+	heightLine(i0 + 500, j1 - 190, i1 + 360, j0 + 10, 1.0f, 0.0f, 0.0f);
+
 	//TODO: try moving object
 }
 int main(void)
